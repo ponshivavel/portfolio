0317a74be0b9ea5579b0d99ed6a0b9e5a54e8b5a
Ponshivavel S K <ponshivavelsk@gmail.com>
2026-02-27 21:34:03 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/protofolio/',
   plugins: [react()],
   server :{
     host:true,port :5173
